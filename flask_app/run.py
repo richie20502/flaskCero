3da9195dd1262f79de09ec87cf916802d8226a3e
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-06 16:45:14 -0600

    archivo de configuraciones de la aplicacion mediante objetos

--- flask_app/run.py
+++ flask_app/run.py
@@ -1,5 +1,9 @@
 from my_app import app
-app.config.from_pyfile('config.py')
+
+#app.config.from_pyfile('config.py')
+app.config.from_object('configuration.DevelopmentConfig')
+
+
 #app.run(debug = True)
 app.run()
 #app.config['debug'] = True
